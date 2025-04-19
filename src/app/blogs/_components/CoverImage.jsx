e5c8ca0c08588c7437debb364fde1c5437426840
zahra nejati <neejati@gmail.com>
2025-04-19 15:03:14 +0330

    cover iamge

---
+++ src/app/blogs/_components/CoverImage.jsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function CoverImage({ coverImageUrl, title, slug }) {
+  return (
+    <div className="relative aspect-video overflow-hidden">
+      <Link href={`/blogs/${slug}`}>
+        <Image
+          src={coverImageUrl}
+          fill
+          className="object-cover object-center hover:scale-110 duration-300 transition-all"
+          alt={title}
+        />
+      </Link>
+    </div>
+  );
+}
--- src/app/blogs/_components/PostList.jsx
+++ src/app/blogs/_components/PostList.jsx
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import React from "react";
+import CoverImage from "./CoverImage";
 
 export default async function PostList() {
   await new Promise((res) => setTimeout(() => res(), 3000));
@@ -9,11 +11,35 @@ export default async function PostList() {
   } = await res.json();
 
   console.log("posts", posts);
-  return (
-    <div>
-      {posts.map((item) => (
-        <div>{item.title}</div>
-      ))}
+  return posts.length > 0 ? (
+    <div className="grid grid-cols-12 gap-8">
+      {posts.map((post) => {
+        return (
+          <div
+            key={post.id}
+            className="col-span-12 sm:col-span-6 md:col-span-6 lg:col-span-6 xl:col-span-4 border border-secondary-100 p-2 rounded-lg"
+          >
+            <CoverImage {...post} />
+
+            <div className="bg-secondary-100  p-2 rounded-lg flex flex-col w-full justify-between flex-1">
+              <Link href={`/blogs/${post.slug}`}>
+                <h2 className="mb-4 font-bold text-secondary-700">
+                  {post.title}
+                </h2>
+              </Link>
+
+              <div className="flex items-center  justify-between mb-4">
+                <div className="flex items-center text-[10px] text-secondary-500">
+                  <ClockIcon className="w-4 h-4 stroke-secondary-500 ml-1" />
+                  <span className="ml-1"> خواندن:</span>
+                  <span className="ml-1 leading-3">{post.readingTime}</span>
+                  <span>دقیقه</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
-  );
+  ) : null;
 }
