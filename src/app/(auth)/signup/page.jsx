48ddefac3753dc0aa4c0ef3c042d1bae11ba0f3b
zahra nejati <neejati@gmail.com>
2025-04-29 13:55:16 +0330

    ....................

---
+++ src/app/(auth)/signup/page.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+export default function Signup() {
+  const [form, setForm] = useState({});
+
+  return (
+    <div>
+      <h2>ثبت نام</h2>
+      <form></form>
+    </div>
+  );
+}
