24cb6e5f65b0c5a73559cd1de8ca0ba56210f13f
zahraNejati <nejatizahra4@gmail.com>
2025-05-02 10:21:12 +0330

    signin form and layout

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
-export default function layout() {
-  return <div>layout</div>;
+export default function layout({ children }) {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="w-full max-w-md p-2">{children}</div>
+    </div>
+  );
 }
--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -1,12 +1,68 @@
+"use client";
+import Button from "@/ui/Button";
+import RHFTextField from "@/ui/RHFTextField";
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const schema = yup
+  .object({
+    name: yup
+      .string()
+      .min(5, "حداقل 5 کاراکتر!")
+      .max(30, "حداکثر 30 کاراکتر!")
+      .required("نام الزامی است!"),
+    email: yup.string().email("ایمیل نا معتبر!").required("ایمیل اجباری است!"),
+    password: yup.string().required("رمز عبور الزامی است!"),
+  })
+  .required();
 
 export default function Signup() {
-  const [form, setForm] = useState({});
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isLoading },
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onTouched",
+  });
+
+  const onSubmit = (values) => {
+    console.log("====================================");
+    console.log(values);
+    console.log("====================================");
+  };
 
   return (
     <div>
       <h2>ثبت نام</h2>
-      <form></form>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-10">
+        <RHFTextField
+          label={"نام و نام خانوادگی"}
+          name={"name"}
+          register={register}
+          isRequired
+          errors={errors}
+        />
+        <RHFTextField
+          label={"ایمیل"}
+          name={"email"}
+          register={register}
+          isRequired
+          errors={errors}
+        />
+        <RHFTextField
+          label={"رمز عبور"}
+          name={"password"}
+          register={register}
+          isRequired
+          errors={errors}
+        />
+        <Button type="submit" variant="primary" className="w-full">
+          تایید
+        </Button>
+      </form>
     </div>
   );
 }
