9826b9d7dc51111cdcf1edd1e48327cc0246f7db
zahraNejati <nejatizahra4@gmail.com>
2025-08-14 00:08:50 +0330

    add search feature to posts page

--- src/app/(dashboard)/profile/posts/_/components/Buttons.jsx
+++ src/app/(dashboard)/profile/posts/_/components/Buttons.jsx
@@ -1,6 +1,17 @@
 import ButtonIcon from "@/ui/ButtonIcon";
-import { PencilIcon, TrashIcon } from "@heroicons/react/24/outline";
+import { PencilIcon, PlusIcon, TrashIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
+
+export function CreatePost() {
+  return (
+    <Link href={`/profile/posts/create`}>
+      <ButtonIcon className={"justify-self-end p-2"} variant={"primary"}>
+        <PlusIcon className="text-primary-100" />
+        <span>ایجاد پست</span>
+      </ButtonIcon>
+    </Link>
+  );
+}
 
 export function DeletePost({ postId }) {
   return (
--- src/app/(dashboard)/profile/posts/page.jsx
+++ src/app/(dashboard)/profile/posts/page.jsx
@@ -1,12 +1,22 @@
 import React, { Suspense } from "react";
 import PostsTable from "./_/components/PostsTable";
 import Fallback from "@/ui/FallBack";
+import { CreatePost } from "./_/components/Buttons";
+import Search from "@/ui/Search";
+import queryString from "query-string";
 
-function page() {
+function page({ searchParams }) {
+  const query = queryString.stringify(searchParams);
+
   return (
     <div>
+      <div className="grid grid-cols-1 lg:grid-cols-3 items-center gap-8 mb-8">
+        <h2>لیست پست ها</h2>
+        <Search />
+        <CreatePost />
+      </div>
       <Suspense fallback={<Fallback />}>
-        <PostsTable />
+        <PostsTable query={query} />
       </Suspense>
     </div>
   );
